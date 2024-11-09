d6ce72bd77ff4651d1ca0c5484fdc5a7d33a74f4
Matt Bryant <brybrant@gmail.com>
2024-11-09 05:55:19 +0000

    lava lamp

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lava Lamp
+
+[Lava Lamp](https://brybrant.github.io/lava-lamp/) is a GLSL experiment made with [TWGL](https://twgljs.org/) and [Vite](https://vitejs.dev) by Matt Bryant.
+
+Fragment shader is based on this [raymarch lavalamp shader](https://www.shadertoy.com/view/fsKXDm) by [@Arrangemonk](https://www.shadertoy.com/user/Arrangemonk)
+
+## Playfair Display (Google Font)
+
+This project uses a subset of ["Playfair Display"](https://github.com/clauseggers/Playfair) which is licensed under the [OFL-1.1 License](https://openfontlicense.org/).
---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint2';
+import glslPlugin from 'vite-plugin-glsl';
+import stylelintPlugin from 'vite-plugin-stylelint';
+import svgoPlugin from 'vite-plugin-svgo';
+
+import * as configs from '@brybrant/configs';
+
+export default defineConfig(({ mode }) => {
+  const development = mode === 'development';
+
+  return {
+    base: '/lava-lamp/',
+    build: {
+      minify: development ? true : 'terser',
+      ...(!development && {
+        terserOptions: configs.terserConfig,
+      }),
+    },
+    css: {
+      postcss: configs.postCSSConfig,
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
+      },
+    },
+    plugins: [
+      stylelintPlugin({
+        lintInWorker: true,
+        config: configs.stylelintConfig,
+      }),
+      glslPlugin({
+        compress: !development,
+      }),
+      svgoPlugin(configs.svgoConfig),
+      eslintPlugin({
+        lintInWorker: true,
+      }),
+    ],
+    server: {
+      host: '127.0.0.1',
+      port: 3000,
+      strictPort: true,
+    },
+  };
+});
